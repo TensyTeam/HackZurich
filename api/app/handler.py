de2ee5c6c15a6c2266e3c6d7a28ed1c148a83ea7
kosyachniy <polozhev@mail.ru>
2019-09-28 15:47:13 +0200

    Distance & time

--- api/app/handler.py
+++ api/app/handler.py
@@ -2,6 +2,8 @@ import json
 
 import googlemaps
 
+from dist2coords import dist2coords
+
 
 with open('keys.json', 'r') as file:
 	KEY = json.loads(file.read())['google']['maps']['key']
@@ -23,29 +25,30 @@ def handler(emojis, time, geo):
 
 		category = CATEGORIES[emoji]
 
+		# Время в расстояние
+
+		radius = dist2coords(80 * time) # m/min * min -> geo dist # 85
+
 		# Преобразование в места
 
 		place = gmaps.find_place(
 			category,
 			'textquery',
 			fields=[
+				'place_id',
 				'geometry/location',
 				'name',
-				# 'formatted_address',
-				# 'photos',
-				# 'price_level',
-				# 'rating',
 			],
-			location_bias='circle:0.5@{},{}'.format(geo['lat'], geo['lng']),
+			location_bias='circle:{}@{},{}'.format(radius, geo['lat'], geo['lng']),
 		)
 
 		geo = place['candidates'][0]['geometry']['location'] # Обновляем текущее местоположение
 
 		points.append({
-			'geo': place['candidates'][0]['geometry']['location'],
+			'id': place['candidates'][0]['place_id'],
 			'name': place['candidates'][0]['name'],
+			'geo': place['candidates'][0]['geometry']['location'],
 			'emoji': emoji,
 		})
-
 
 	return points
